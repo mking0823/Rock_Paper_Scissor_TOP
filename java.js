c67d7cc20653651ea19961f48eea4a7fc5d9f0e5
mking0823 <101141697+mking0823@users.noreply.github.com>
2022-05-15 10:03:18 -0400

    Add files via upload
    Adding Initial Files. I started this project on my windows machine and am moving it to Linux so I can properly use git as it is not set up on my windows system.

---
+++ java.js
@@ -0,0 +1,49 @@
+
+
+
+// Global Variables
+let playerScore = 0;
+let ComputerScore =0;
+
+
+let randomNumber = Math.floor(Math.random() * 2)  //NEXT 3 LINES RADOMIZES COMPUTER CHOICES
+
+const computerChoices = ["SCISSORS", "PAPER", "ROCK"];
+
+computerMove = (computerChoices[randomNumber])
+ 
+
+
+let playerInput = prompt("ROCK PAPER SCISSOR"); // Get users Choice
+
+let playerMove = playerInput.toUpperCase();  // Make input uniform
+
+
+playRound(playerMove, computerMove) 
+
+ function playRound(playerMove, computerMove){
+    if ( computerMove == playerMove) //Draw condition 
+    alert("it is a tie");
+
+    else if (playerMove == "ROCK" && computerMove == "SCISSORS") //Win Condition 
+    alert("ROCK BEATS SCISSORS. YOU WIN!!!");
+    
+    else if ( playerMove == "PAPER" && computerMove == "ROCK")
+    alert("PAPER BEATS ROCK. YOU WIN!!!");
+    
+    else if (playerMove == "SCISSORS" && computerMove == "PAPER")
+    alert("SCISSORS BEATS PAPER. YOU WIN!!!"); 
+    
+    else                     //lose Conidtion
+    alert(`YOU LOSE!!! ${computerMove} BEATS ${playerMove}.`   );}
+
+
+ 
+ 
+   
+    
+ 
+
+    
+    console.log(computerMove, playerMove)
+    console.log(randomNumber)
